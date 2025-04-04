c9504137f8af0b22ce29fbd3f0ace4e607c7574b
Sunny Kumar <sk202542@gmail.com>
2025-04-05 01:46:15 +0530

    day 8

---
+++ day 8 Arrays/app.js
@@ -0,0 +1,70 @@
+// const arr =[1,2,3,4,"hello",true,null]
+
+// console.log(arr[3]);
+// console.log("Array length",arr.length);
+
+// for(let i = 0 ; i<arr.length;i++){
+//     console.log(`array of ${i} is ${arr[i]}`);  
+// }
+
+// for(let i of arr){
+//     console.log(`array of ${i}`);  
+// }
+
+// const arr = new Array(7)
+// arr[6]=5;
+// arr[0]="sunny"
+
+// console.log(arr);
+
+const arr = [2,2,5,55,12,10,54]
+// const joinMe=arr.join(",")
+// console.log(typeof(joinMe));
+
+// const newArr = joinMe.split(",")
+// console.log(newArr);
+
+delete arr[4]
+console.log(arr);
+
+
+
+// let checkSome = arr.some(function (i){
+//     if(i>5)return true
+//     else return false
+// })
+
+// let checkEvery = arr.every(function (i){
+//     if(i>5)return true
+//     else return false
+// })
+
+// let checkFind = arr.find(function (i){
+//     return i>5
+// })
+//console.log(arr.sort((a,b)=>b-a));
+
+
+
+
+
+// arr.push(50)
+// console.log(arr);
+// arr.pop()
+// arr.pop()
+// console.log(arr);
+// const newArr = arr.splice(2,2)
+//console.log(newArr);
+// arr.splice(2,2,5,6,8)
+
+// arr.reverse()
+
+// const newArr = arr.slice(0,5)
+
+// console.log(newArr);
+
+
+
+
+
+
